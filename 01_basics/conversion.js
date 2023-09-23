861807b764d3212b7335cb00b0ed10a260358d0b
shivammathur08 <145829567+shivammathur08@users.noreply.github.com>
2023-09-23 16:17:01 +0000

    container config done

---
+++ 01_basics/conversion.js
@@ -0,0 +1,11 @@
+"use strict";
+/*let scoreTotal = "Shivam"
+console.log(typeof scoreTotal);
+let scoreResult =   Number(scoreTotal)
+console.log(scoreResult)
+console.log(typeof scoreResult)
+*/ // conversion
+
+let isLoggedIn = 44nn
+let isLogged = Boolean(isLoggedIn)
+console.log(isLogged);
