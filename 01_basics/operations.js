16f368f93e6073b0dd43069602f98d41e64b7c34
shivammathur08 <145829567+shivammathur08@users.noreply.github.com>
2023-09-23 16:55:50 +0000

    container config done

---
+++ 01_basics/operations.js
@@ -0,0 +1,25 @@
+"use strict";
+/*console.log("3"-null)
+console.log(3*3);
+console.log(3**3);
+console.log(33%4);
+console.log(3+3)
+console.log("3" + "3");
+console.log("3" + 3);
+console.log("3"-"3");
+*/
+/*let str1 = "sHIVAM"
+let str2 = "mathur"
+console.log(str1 + " " + str2);
+
+let str3 = "Shivam"
+let str4 = 2
+console.log(str3 + str4);
+console.log("1"+2+2);
+console.log(1+4+4+"4");
+console.log(2+3+"5"+5+5);
+console.log("5"+5+5+"6");
+*/
+let counterValue = 1000
+ let counterASER = counterValue--;
+console.log(counterASER);
