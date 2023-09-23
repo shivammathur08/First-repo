a1088a4c6dc9f54c3e1d85f7ad56c0594bd82432
shivammathur08 <145829567+shivammathur08@users.noreply.github.com>
2023-09-23 09:43:33 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,18 @@
+//constant variable
+const accountId = 6165550
+//accountId = 22222
+console.log(accountId)
+
+// Difference between var and let
+/*{
+    var hello = "Levi is the best side character"
+    console.log(hello)
+}*/
+//console.log(hello)
+{
+    let hello = "Eren  is right" //let is block scoped which means it cannot be accessed outside the block. We have to redeclare it in order to use it.
+    console.log(hello)
+}
+//console.log(hello)
+let accountState;
+console.log(accountState)
