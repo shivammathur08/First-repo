329dea4b3b0871b4b005f7657c3d5a45e2c233ba
shivammathur08 <145829567+shivammathur08@users.noreply.github.com>
2023-09-24 15:13:42 +0000

    container config done

---
+++ 01_basics/comparison.js
@@ -0,0 +1,14 @@
+//console.log("3">"2")
+//console.log(2==3);
+//console.log(null = 0)
+/*console.log(null>undefined)
+console.log(null>=0)
+console.log(null<=0)*/
+
+/*console.log(undefined >0)
+console.log(undefined <0)
+console.log(undefined >=0)
+console.log(undefined <=0)*/
+
+// ===
+console.log("3"===3)
---
+++ 01_basics/dynamically.js
@@ -0,0 +1,31 @@
+// Javascript is dynamically typed language because it will assign the type of datatype for each variable during code execution.
+
+/*function a()
+{
+    console.log("Shivam")
+}
+a();  //it holds the string
+a=66  // here we assign a number to it
+a();*/
+
+/*function a()
+{
+    b = 10+13
+    console.log(b)
+}
+a() //function call*/
+
+function a(b,c)
+{
+    if(b<c)
+    {
+        d = b+c
+        console.log(d)
+    }
+   else
+   {
+    console.log("Dumbo")
+   }
+}
+a(100,66)
+
